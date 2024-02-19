6de0816207f2fd9e48e9b992260bd5e931044c64
IntellergyDevs <devcloud@intellergy.co.za>
2024-02-19 13:58:47 +0200

    created jest.config.cjs

---
+++ frontend/jest.setup.js
@@ -0,0 +1,14 @@
+
+// jest.setup.js
+// Add any global setup, for example with jest-extended or enzyme, etc.
+
+// Example of extending expect with jest-extended
+require('jest-extended');
+
+// Example of setting up enzyme with Adapter for React 16
+// const Enzyme = require('enzyme');
+// const Adapter = require('enzyme-adapter-react-16');
+
+// Enzyme.configure({ adapter: new Adapter() });
+
+// You can add any global setup you need for Jest here.
