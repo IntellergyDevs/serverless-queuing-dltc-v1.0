4a25d9d402e408a1e74811e5f6b6742519e0c837
IntellergyDevs <devcloud@intellergy.co.za>
2024-02-19 14:44:09 +0200

    test.jsx

--- frontend/src/babel.config.js
+++ frontend/src/babel.config.js
@@ -1,3 +1,11 @@
+// babel.config.js
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"]
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react" // This preset is necessary for JSX transformation
+  ],
+  // If you're using any plugins, they would go here
+  plugins: [
+    // If you have custom Babel plugins, they would be listed here
+  ]
 };
