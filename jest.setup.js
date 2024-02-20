b0d86f6a0aa6b8ebdf43d204bc75094a2f14d946
IntellergyDevs <devcloud@intellergy.co.za>
2024-02-20 15:39:21 +0200

    jest file

---
+++ jest.setup.js
@@ -0,0 +1,14 @@
+// jest.setup.js
+
+// Example: Mocking a global function
+global.console.log = jest.fn();
+
+// Example: Setting up test data
+beforeAll(() => {
+  // Setup test data or perform any necessary initialization
+});
+
+// Example: Cleaning up after tests
+afterAll(() => {
+  // Clean up test data or perform any necessary teardown
+});
